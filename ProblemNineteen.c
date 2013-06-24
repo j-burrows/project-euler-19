f9963a49020c4d964940b5ceed367393b43622c4
j-burrows <gameaddress1@hotmail.ca>
2013-06-24 03:47:18 -0700

    V1.1: Formatted comments and documents

--- ProblemNineteen.c
+++ ProblemNineteen.c
@@ -1,15 +1,13 @@
-/* ProblemNineteen.c*/
+/*-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Filename:	ProblemNineteen.c
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+*/
 #include "ProblemNineteen.h"
 
-/* function:	leap_year
- * @param:	year, the year that will be determined as a leap year or not.
- * @return:	is_leap_year, will be 1 if given year is a leap year, zero if not.
- *
- * Checks to see if the given year divisible by 4. If not, it is automatically not a leap year. If it is divisible
- * by four, it checks to see if it is divisible by 100 and 400. If divisible by 4 and 100 but not by 400, it is not a leap year.
- * If it is divisible by 4; or divisible by 4, 100, and 400, it is a leap year. One is returned if it is a leap year, zero
- * if it is not.
- */
+/*-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Function:	leap_year
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+*/
 int leap_year(int year){
 	int is_leap_year;
 	if((year%4) !=0){
@@ -26,15 +24,10 @@ int leap_year(int year){
 	return is_leap_year;
 }
 
-/* Subroutine:	add_month
- * @param:	day, a pointer to the day of the week, will be changed depending on month
- * 			month, a pointer to the month, will be incremented by one and set to 1 if the result is over 12
- *			year, a pointer to the year, will be incremented if the incrimented month exceeds 12.
- *
- * Adds to days the length of the current month. Such that April, June, September, and November have 30 day lengths, 
- * febuary is checked to be a leap year, adding 28 if not, 29 if it is; and the rest add 31. The days are modded with 
- * seven, and the month is incrimented. If the resulting month is over 12, it is set to 1, and the year is incrimented.
- */
+/*-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Function:	add_month
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+*/
 void add_month(int* day, int* month, int* year){
 	if( (*month) == 4 || (*month) == 6 || (*month) == 9 || (*month) == 11){
 		(*day) += 30;
@@ -59,13 +52,10 @@ void add_month(int* day, int* month, int* year){
 	}
 }
 
-/* Subroutine:	first_of_month_sundays
- * @return:	sundays, the number of sundays that lie on the first of a month between jan 1st 1901 and dec 31st 2001.
- *
- * The date is initialised to the first of january, 1900. Then the date is continuosly incremented by 1 month to the year 2001.
- * After every incriment, if the date falls between jan 1st 1901 and dec 31st 2001, the day is checked to be a sunday. If it is
- * the number of sundays is incrimented by one. After reaching the year 2001 the total sundays are returned.
- */
+/*-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Function:	add_month
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+*/
 int first_of_month_sundays(){
 	int day, month, year, sundays;
 	day = 2;
@@ -84,8 +74,9 @@ int first_of_month_sundays(){
 	return sundays;
 }
 
-/* This program will calculate and display the number of sundays that lie on the first of the month, between Janurary 1st
- * 1901 and December 31st 2001. It will then close after a five second wait.*/
+/* This program will calculate and display the number of sundays that lie on the first of 
+   the month, between Janurary 1st 1901 and December 31st 2001. It will then close after 
+   a five second wait.*/
 int main(){
 	int sundays;
 	int countdown;
--- ProblemNineteen.h
+++ ProblemNineteen.h
@@ -1,6 +1,14 @@
-/* ProblemNineteen.h
- * 
- * Purpose: To have a program that calculates and displays how many sundays fell on the first  of a month  */
+/*-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Filename:	ProblemNineteen.h
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Purpose:	To have a program that calculates and displays how many sundays fell on the 
+ |				first of a month
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Author:		Jonathan Burrows
+ |	Date:		November 12th 2012
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+*/
+
 #ifndef PROBLEMNINETEEN_H
 #define PROBLEMNINETEEN_H 1
 #include <stdio.h>
@@ -9,13 +17,55 @@
 /*sleep waits for one second for every x*/
 #define sleep(x) Sleep(1000*x)
 
-/*Returns a 1 if the given year is a leap year, zero if not.*/
+/*-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Function:	leap_year
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Purpose:	Returns a 1 if the given year is a leap year, zero if not.
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	@param:		year, 		the year that will be determined as a leap year or not.
+ | 	@return:	is_leap_year, will be 1 if given year is a leap year, zero if not.
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Algorithm:	Checks to see if the given year divisible by 4. If not, it is automatically 
+ |				not a leap year. If it is divisible by four, it checks to see if it is 
+ |				divisible by 100 and 400. If divisible by 4 and 100 but not by 400, it is 
+ |				not a leap year. If it is divisible by 4; or divisible by 4, 100, and 400, 
+ |				it is a leap year. One is returned if it is a leap year, zeroif it is not.
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+*/
 int leap_year(int year);
 
-/* incriments the given date by 1 month.*/
+/*-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Function:	add_month
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Purpose:	incriments the given date by 1 month.
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	@param:		year, 		the year that will be determined as a leap year or not.
+ | 	@return:	is_leap_year, will be 1 if given year is a leap year, zero if not.
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Algorithm:	Checks to see if the given year divisible by 4. If not, it is automatically 
+ |				not a leap year. If it is divisible by four, it checks to see if it is 
+ |				divisible by 100 and 400. If divisible by 4 and 100 but not by 400, it is 
+ |				not a leap year. If it is divisible by 4; or divisible by 4, 100, and 400, 
+ |				it is a leap year. One is returned if it is a leap year, zero if it is not.
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+*/
 void add_month(int* day, int* month, int* year);
-
-/* Counts all the sundays that lie on the first of a month between January 1st 1901 and December 31st 2001*/
+/*-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Function:	add_month
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Purpose:	Counts all the sundays that lie on the first of a month between January 1st 
+ |				1901 and December 31st 2001
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	@return:	sundays, 	the number of sundays that lie on the first of a month between  
+ |							jan 1st 1901 and dec 31st 2001.
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+ |	Algorithm:	The date is initialised to the first of january, 1900. Then the date is 
+ |				continuosly incremented by 1 month to the year 2001. After every incriment, 
+ |				if the date falls between jan 1st 1901 and dec 31st 2001, the day is checked 
+ |				to be a sunday. If it is the number of sundays is incrimented by one. After 
+ |				reaching the year 2001 the total sundays are returned.
+ +-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+*/
 int first_of_month_sundays();
 
 #endif
